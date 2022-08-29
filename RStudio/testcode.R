0cf92a4b88198d764598bf71feaf390f120e90d4
santi9006 <s.de47@uniandes.edu.co>
2022-08-29 14:52:58 -0500

    Revert "Ac R"
    This reverts commit ead9388fa89c2e769749b1f85bcbba26f7f1a9a8.

--- RStudio/testcode.R
+++ RStudio/testcode.R
@@ -4,10 +4,5 @@ cat("\014")
 local({r <- getOption("repos"); r["CRAN"] <- "http://cran.r-project.org";options(repos=r)}) #set repo
 
 require(tidyverse)
-require(gtsummary)
 
 dta<-readRDS("file.Rds")
-
-#Voy a agregar estadísticas descriptivas
-
-gtsummary(data(name))
