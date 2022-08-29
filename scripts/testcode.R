ead9388fa89c2e769749b1f85bcbba26f7f1a9a8
santi9006 <s.de47@uniandes.edu.co>
2022-08-29 14:51:35 -0500

    Ac R

--- scripts/testcode.R
+++ scripts/testcode.R
@@ -4,5 +4,10 @@ cat("\014")
 local({r <- getOption("repos"); r["CRAN"] <- "http://cran.r-project.org";options(repos=r)}) #set repo
 
 require(tidyverse)
+require(gtsummary)
 
 dta<-readRDS("file.Rds")
+
+#Voy a agregar estadísticas descriptivas
+
+gtsummary(data(name))
