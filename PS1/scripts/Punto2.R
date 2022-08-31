ab2b14aa09cf27f1903c9574036416f87f7899a7
santi9006 <111027027+santi9006@users.noreply.github.com>
2022-08-31 18:50:16 -0500

    Punto 2 - Regresión
    Hola, paso mis ideas del punto 2.

---
+++ PS1/scripts/Punto2.R
@@ -0,0 +1,27 @@
+## Punto 2##
+
+### 2.1 Cargar paquetes y base de datos#
+install.packages("pacman")
+require(pacman)
+p_load(tidyverse,
+       rvest,
+       here,
+       jtools, ## summ function
+       ggstance, ## 
+       broom, ## tidy function (Saca tabla lindas)
+       broom.mixed, ##
+       skimr,
+       expss,
+       dplyr,
+       vtable,
+       gtsummary) 
+
+setwd("/Users/SantyDLC/Dropbox (Personal)/Doctorado Uniandes/202220/Machine Learning/GitHub/BigData_Grupo_2022/PS1/stores")
+use(ps1_trim.Rda)
+
+## 2.2. Regresi??n y gr??fica
+
+m1<-lm(y_total_m_ha~age+c(age*age), ps1_trim)
+effect_plot(model=m1, pred = age, interval=TRUE)
+
+
